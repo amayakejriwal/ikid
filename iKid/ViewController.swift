25d9f0c9aba19a5a82b5408b43d055d642eabecc
amayakejriwal <amayak@uw.edu>
2022-04-30 21:36:53 -0700

    first commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Amaya Kejriwal on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
